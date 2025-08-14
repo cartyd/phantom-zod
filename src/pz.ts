9e6cbd4a1a8cc0d24b93fc73771b701989f75b5d
Dave <9923782+cartyd@users.noreply.github.com>
2025-08-14 13:07:55 -0400

    feat: add pz barrel file to export all schemas under the pz namespace

---
+++ src/pz.ts
@@ -0,0 +1,40 @@
+// Barrel file to export all schemas under the `pz` namespace
+import * as addressSchemas from './schemas/address-schemas';
+import * as arraySchemas from './schemas/array-schemas';
+import * as booleanSchemas from './schemas/boolean-schemas';
+import * as dateSchemas from './schemas/date-schemas';
+import * as emailSchemas from './schemas/email-schemas';
+import * as enumSchemas from './schemas/enum-schemas';
+import * as fileUploadSchemas from './schemas/file-upload-schemas';
+import * as idListSchemas from './schemas/id-list-schemas';
+import * as moneySchemas from './schemas/money-schemas';
+import * as networkSchemas from './schemas/network-schemas';
+import * as numberSchemas from './schemas/number-schemas';
+import * as paginationSchemas from './schemas/pagination-schemas';
+import * as phoneSchemas from './schemas/phone-schemas';
+import * as postalCodeSchemas from './schemas/postal-code-schemas';
+import * as stringSchemas from './schemas/string-schemas';
+import * as urlSchemas from './schemas/url-schemas';
+import * as userSchemas from './schemas/user-schemas';
+import * as uuidSchemas from './schemas/uuid-schemas';
+
+export const pz = {
+  ...addressSchemas,
+  ...arraySchemas,
+  ...booleanSchemas,
+  ...dateSchemas,
+  ...emailSchemas,
+  ...enumSchemas,
+  ...fileUploadSchemas,
+  ...idListSchemas,
+  ...moneySchemas,
+  ...networkSchemas,
+  ...numberSchemas,
+  ...paginationSchemas,
+  ...phoneSchemas,
+  ...postalCodeSchemas,
+  ...stringSchemas,
+  ...urlSchemas,
+  ...userSchemas,
+  ...uuidSchemas,
+};
